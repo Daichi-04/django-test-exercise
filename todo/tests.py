d6a4b151cabb1665f27ce12883596ce03e32e5e2
1f10230300 <s1f102303002@iniad.org>
2024-07-04 02:18:07 +0900

    ソート機能を追加

--- todo/tests.py
+++ todo/tests.py
@@ -52,4 +52,29 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name,'todo/index.html')
         self.assertEqual(len(response.content['tasks']),1)
-        
+
+    def test_index_get_order_post(self):
+        task1 = Task(title='task1',due_at=timezone.make_aware(datetime(2024,7,1)))
+        task1.save()
+        task2 = Task(title='task2',due_at=timezone.make_aware(datetime(2024,8,1)))
+        task2.save()
+        client = Client()
+        resonse = client.get('/?order=post')
+
+        self.assertEqual(resonse.status_code,200)
+        self.assertEqual(resonse.templates[0].name,'todo/index.html')
+        self.assertEqual(resonse.context['tasks'][0],task2)
+        self.assertEqual(resonse.context['tasks'][1],task1)
+    
+    def  test_index_get_order_due(self):
+        task1 = Task(title='task1',due_at=timezone.make_aware(datetime(2024,7,1)))
+        task1.save()
+        task2 = Task(title='task2',due_at=timezone.make_aware(datetime(2024,8,1)))
+        task2.save()
+        client = Client()
+        resonse = client.get('/?order=post')
+
+        self.assertEqual(resonse.status_code,200)
+        self.assertEqual(resonse.templates[0].name,'todo/index.html')
+        self.assertEqual(resonse.context['tasks'][0],task1)
+        self.assertEqual(resonse.context['tasks'][1],task2)
