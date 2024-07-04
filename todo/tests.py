6e2d530747e68ae35278046484865d045df71efb
1f10230300 <s1f102303002@iniad.org>
2024-07-04 13:56:45 +0900

    ソート機能を修正

--- todo/tests.py
+++ todo/tests.py
@@ -42,7 +42,7 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name,'todo/index.html')
-        self.assertEqual(len(response.content['tasks']),0)
+        self.assertEqual(len(response.context['tasks']),0)
 
     def test_index_post(self):
         client = Client()
@@ -51,7 +51,7 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name,'todo/index.html')
-        self.assertEqual(len(response.content['tasks']),1)
+        self.assertEqual(len(response.context['tasks']),1)
 
     def test_index_get_order_post(self):
         task1 = Task(title='task1',due_at=timezone.make_aware(datetime(2024,7,1)))
